c1c65a704bc6ccf3a07ed4aae0e34d50a41b68e2
Nitipat L <nitipat@igeargeek.com>
2021-05-07 18:45:04 +0700

    [Edited] add example of looping

--- basic/main.go
+++ basic/main.go
@@ -4,7 +4,13 @@ import "fmt"
 
 func main() {
 	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for _, card := range cards {
+		fmt.Println(card)
+	}
 	fmt.Println(cards)
+
 }
 
 func newCard() string {
