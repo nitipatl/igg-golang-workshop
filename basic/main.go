b25c9b4ecc9c933b7701f826d1319a5cf7bb6071
Nitipat L <nitipat@igeargeek.com>
2021-05-11 18:10:02 +0700

    [Edited] move to function

--- basic/main.go
+++ basic/main.go
@@ -1,20 +1,6 @@
 package main
 
 func main() {
-	cards := deck{}
-	cardSuits := []string{"Diamonds", "Hearts", "Spades", "Clubs"}
-	cardValues := []string{
-		"Ace",
-		"Two", "Three", "Four", "Five", "Six",
-		"Seven", "Eight", "Nine", "Ten",
-		"Jack", "Queen", "King",
-	}
-
-	for _, suit := range cardSuits {
-		for _, value := range cardValues {
-			cards = append(cards, value+" of "+suit)
-		}
-	}
-
+	cards := newDeck()
 	cards.print()
 }
