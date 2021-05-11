4e5a3f4b6c0ff999dd80f0626828151a54094a79
Nitipat L <nitipat@igeargeek.com>
2021-05-11 18:07:56 +0700

    [Edited] add loop of card

--- basic/main.go
+++ basic/main.go
@@ -2,10 +2,19 @@ package main
 
 func main() {
 	cards := deck{}
-	// Logic : card adding
-	cards.print()
-}
+	cardSuits := []string{"Diamonds", "Hearts", "Spades", "Clubs"}
+	cardValues := []string{
+		"Ace",
+		"Two", "Three", "Four", "Five", "Six",
+		"Seven", "Eight", "Nine", "Ten",
+		"Jack", "Queen", "King",
+	}
 
-func newCard() string {
-	return "Ace of Spades"
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	cards.print()
 }
